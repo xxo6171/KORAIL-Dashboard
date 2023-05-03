2ae464363634cf2998b70cba36339901e69c285f
KyungminLEE <kyung971112@gmail.com>
2023-05-03 09:55:09 +0900

    Add. 쓰레드 함수 추가
    - 각 인터페이스의 쓰레드 클래스 생성 후 값 업데이트

--- main.py
+++ main.py
@@ -28,6 +28,16 @@ class MainWindow(QMainWindow):
         self.ui_list = self.ui.getUiList()
         self.connectClickUi(self.ui_list, self.ui.pushButton)
 
+        # Create ui(interface)'s threads
+        self.t1, self.t2, self.t3, self.t4, self.t5, \
+            self.t6, self.t7, self.t8, self.t9, self.t10, self.t11 = Thread1(self), Thread2(self), Thread3(self), Thread4(self), Thread5(self), \
+            Thread6(self), Thread7(self), Thread8(self), Thread9(self), Thread10(self), Thread11(self)
+
+        self.thread_list = [self.t1, self.t2, self.t3, self.t4, self.t5,
+                            self.t6, self.t7, self.t8, self.t9, self.t10, self.t11]
+
+        self.run(self.ui_list, self.thread_list)
+
     # UI 클릭 이벤트 처리
     def connectClickUi(self, ui_list, button) -> None:
         for ui in ui_list:
@@ -52,6 +62,19 @@ class MainWindow(QMainWindow):
 
         stack.setCurrentIndex(0)
         chart.updateChart()
+
+    # todo: Executing Interface thread
+    def run(self, ui_list: list, thread_list: list):
+        for _thr, _ui in zip(thread_list, ui_list):
+            _thr.progress.connect(partial(self.updateInterface, _ui, None if _ui.objectName() != 'widget_4' else 'inverse'))
+
+        for _thr in thread_list:
+            _thr.start()
+
+    # todo: Receive from interface thread signal
+    @Slot(int)
+    def updateInterface(self, obj: object, flag: str, value: int):
+        obj.updateValue(value, flag=flag)
 
 # 폰트 크기 고정 ( 화면 크기가 다를 시 발생 하는 문제 해결 )
 def suppress_qt_warnings() -> None:
